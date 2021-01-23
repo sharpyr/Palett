5680aad2c4d4757230e2639137ffac4cb98a870f
hoyeungw <hoyeungw@outlook.com>
2021-01-24 00:34:28 +0800

    add ColorConverter

--- Palett.Convert/Src/ColorConverter.cs
+++ Palett.Convert/Src/ColorConverter.cs
@@ -1,5 +1,21 @@
-﻿namespace Palett.Convert {
+﻿using System.Drawing;
+using RGB = System.ValueTuple<byte, byte, byte>;
+using HSL = System.ValueTuple<float, float, float>;
+
+namespace Palett.Convert {
   public partial class ColorConverter {
-    
+    public static Color RgbToColor(RGB rgb) {
+      var (r, g, b) = rgb;
+      return Color.FromArgb(r, g, b);
+    }
+
+    public static RGB ColorToRgb(Color color) => (color.R, color.G, color.B);
+
+    public static Color HslToColor(HSL hsl) {
+      var (r, g, b) = hsl.HslToRgb();
+      return Color.FromArgb(r, g, b);
+    }
+
+    public static HSL ColorToHsl(Color color) => (color.GetHue(), color.GetSaturation() * 100, color.GetBrightness() * 100);
   }
 }
