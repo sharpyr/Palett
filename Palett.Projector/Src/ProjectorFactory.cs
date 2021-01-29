d756c7ae3a75edb77ce6ff0b87a23b723517ab26
hoyeungw <hoyeungw@outlook.com>
2021-01-29 14:07:03 +0800

    add ProjectorFactory

---
+++ Palett.Projector/Src/ProjectorFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Palett.Convert;
+using Palett.Dye;
+using Palett.Projector.Utils;
+using Palett.Utils.Types;
+using HSL = System.ValueTuple<float, float, float>;
+
+namespace Palett.Projector {
+  public class ProjectorFactory {
+    public float Floor;
+    public HSL Lever;
+    public HSL Basis;
+    public DyeFactory<HSL> Factory;
+    public HSL Default;
+
+    public static ProjectorFactory Build((float min, float max) bound, Preset preset, params Effect[] effects) {
+      var colorLeap = preset.PresetToLeap();
+      return new ProjectorFactory {
+        Floor = bound.min,
+        Lever = colorLeap.dif.Div(bound.max - bound.min),
+        Basis = colorLeap.min,
+        Factory = DyeFactory.Hsl(effects),
+        Default = Converter.HexToHsl(preset.Na)
+      };
+    }
+
+    public HSL Project(float val) {
+      if (float.IsNaN(val)) { return this.Default; }
+      var floor = this.Floor;
+      var (leverH, leverS, leverL) = this.Lever;
+      var (baseH, baseS, baseL) = this.Basis;
+      return (
+        Util.Scale(val, floor, leverH, baseH, 360),
+        Util.Scale(val, floor, leverS, baseS, 100),
+        Util.Scale(val, floor, leverL, baseL, 100)
+      );
+    }
+
+    public string Render(float value, string text) => this.Factory.Render(this.Project(value), text);
+
+    public Func<string, string> Make(float value) => this.Factory.Make(this.Project(value));
+
+    public Func<string, string> MakeDefault() => this.Factory.Make(this.Default);
+  }
+}
--- Palett.Projector/Src/Utils/Util.cs
+++ Palett.Projector/Src/Utils/Util.cs
@@ -5,13 +5,13 @@ using Palett.Utils.Types;
 using HSL = System.ValueTuple<float, float, float>;
 
 namespace Palett.Projector.Utils {
-  public static class Utils {
+  public static class Util {
     public static (TO, TO, TO) Map<T, TO>(this (T x, T y, T z) t, Func<T, TO> f) => (f(t.x), f(t.y), f(t.z));
 
     public static Func<string, string> preset_to_flat(this Preset preset, params Effect[] effects) =>
       DyeFactory.Hex(effects).Make(preset.Na);
 
-    public static (HSL min, HSL dif, HSL max) preset_to_leap(this Preset preset) {
+    public static (HSL min, HSL dif, HSL max) PresetToLeap(this Preset preset) {
       var max = Converter.HexToHsl(preset.Max);
       var min = Converter.HexToHsl(preset.Min);
       return (
@@ -20,7 +20,9 @@ namespace Palett.Projector.Utils {
         max
       );
     }
-    public static double Scale(double value, double floor, double lever, double basis, double ceil) =>
+    public static float Scale(float value, float floor, float lever, float basis, float ceil) =>
       Math.Min((Math.Max(value, floor) - floor) * lever + basis, ceil);
+
+    public static HSL Div(this (float x, float y, float z) t, float other) => (t.x / other, t.y / other, t.z / other);
   }
 }
