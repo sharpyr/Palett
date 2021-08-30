4b1970f40fc1258278e9fb1e1d2cc8e8f47dd4a9
hoyeungw <hoyeungw@outlook.com>
2021-08-30 09:47:17 +0800

    add AlmostEqual to compare between hsl and polar

--- Palett.Munsell/Src/Munsell.Utils.cs
+++ Palett.Munsell/Src/Munsell.Utils.cs
@@ -1,11 +1,12 @@
-﻿using static System.Math;
+﻿using Aryth;
+using static System.Math;
 using HSL = System.ValueTuple<float, float, float>;
 using RGB = System.ValueTuple<byte, byte, byte>;
 
 namespace Palett {
   public static partial class Munsell {
     public static (byte r, byte g, byte b) Relative(this (byte r, byte g, byte b) rgb, (byte r, byte g, byte b) sub) {
-      return ((byte) Abs(rgb.r - sub.r), (byte) Abs(rgb.g - sub.g), (byte) Abs(rgb.b - sub.b));
+      return ((byte)Abs(rgb.r - sub.r), (byte)Abs(rgb.g - sub.g), (byte)Abs(rgb.b - sub.b));
     }
     public static (float h, float s, float l) Relative(this (float h, float s, float l) hsl, (float h, float s, float l) sub) {
       return (Distance(hsl.h, sub.h), Abs(hsl.s - sub.s), Abs(hsl.l - sub.l));
@@ -28,6 +29,11 @@ namespace Palett {
     }
     public static bool AlmostEqual(this (float h, float s, float l) hsl, (float h, float s, float l) sub, (float h, float s, float l) epsilon) {
       return Distance(hsl.h, sub.h) < epsilon.h && Abs(hsl.s - sub.s) < epsilon.s && Abs(hsl.l - sub.l) < epsilon.l;
+    }
+    public static bool AlmostEqual(this (float h, float s, float l) hsl, (double r, double θ) polar, (double r, double θ) polarEpsilon, (double min, double max) saturationInterval) {
+      return Math.AlmostEqual(hsl.h, polar.θ, polarEpsilon.θ) &&
+             saturationInterval.Has(hsl.s) &&
+             Math.AlmostEqual(hsl.l, polar.r, polarEpsilon.r);
     }
   }
 }
