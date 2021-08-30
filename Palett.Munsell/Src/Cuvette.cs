d8d88af9c7c256e44122631c4a2ed3970d2559d4
hoyeungw <hoyeungw@outlook.com>
2021-08-30 09:43:59 +0800

    changed Cuvette from static class to class, allowing specification of Domain

--- Palett.Munsell/Src/Cuvette.cs
+++ Palett.Munsell/Src/Cuvette.cs
@@ -1,64 +1,56 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Veho.Enumerable;
+using Veho.Sequence;
 using RGB = System.ValueTuple<byte, byte, byte>;
 using HSL = System.ValueTuple<float, float, float>;
+using POLAR = System.ValueTuple<double, double>;
 using HEX_RGB = System.ValueTuple<string, System.ValueTuple<byte, byte, byte>>;
 using HEX_HSL = System.ValueTuple<string, System.ValueTuple<float, float, float>>;
+using HEX_POLAR = System.ValueTuple<string, System.ValueTuple<double, double>>;
 
 namespace Palett {
-  public static class Cuvette {
-    private static List<HEX_RGB> _hexToRgb = null;
-    private static List<HEX_HSL> _hexToHsl = null;
-    public static List<(string hex, RGB rgb)> HexToRgb => _hexToRgb != null && _hexToRgb.Any()
+  public class Cuvette {
+    public Dictionary<string, string> Dict;
+    private List<(string key, string val)> _list = null;
+    private List<HEX_RGB> _hexToRgb = null;
+    private List<HEX_HSL> _hexToHsl = null;
+    private List<HEX_POLAR> _hexToPolar = null;
+    public static Cuvette Build(Dictionary<string, string> raw) => new Cuvette { Dict = raw };
+    public string this[string key] => this.Dict[key];
+    public List<(string key, string val)> List => _list != null && _list.Any()
+      ? _list
+      : _list = Dict.Select(kv => (kv.Key, kv.Value)).ToList();
+    public List<(string hex, RGB rgb)> HexToRgb => _hexToRgb != null && _hexToRgb.Any()
       ? _hexToRgb
-      : _hexToRgb = Pavtone.HexToName.Map(kv => (kv.Key, Conv.HexToRgb(kv.Key))).ToList();
-    public static List<(string hex, HSL hsl)> HexToHsl => _hexToHsl != null && _hexToHsl.Any()
+      : _hexToRgb = Dict.Map(kv => (kv.Key, Conv.HexToRgb(kv.Key))).ToList();
+    public List<(string hex, HSL hsl)> HexToHsl => _hexToHsl != null && _hexToHsl.Any()
       ? _hexToHsl
       : _hexToHsl = HexToRgb.Map(kv => (kv.hex, kv.rgb.RgbToHsl())).ToList();
+    public List<(string hex, POLAR polar)> HexToPolar => _hexToPolar != null && _hexToPolar.Any()
+      ? _hexToPolar
+      : _hexToPolar = HexToHsl.Map(kv => (kv.hex, kv.hsl.HslToPolar())).ToList();
 
-    public static List<(string hex, string name)> Search(string name) {
-      var regex = new Regex(name, RegexOptions.IgnoreCase);
+    public (string hex, string name) Nearest(HSL hsl) {
+      var (hex, _) = this.HexToHsl.MinBy(kv => Munsell.Distance(hsl, kv.hsl));
+      return (hex, this[hex]);
+    }
+    public (string hex, string name) Nearest(POLAR polar, float s) {
+      var hsl = polar.PolarToHsl(s);
+      var (hex, _) = this.HexToHsl.MinBy(kv => Munsell.Distance(hsl, kv.hsl));
+      return (hex, this[hex]);
+    }
+  }
 
-      var selected = Pavtone.HexToName
-                            .ToList()
-                            .FindAll(kv => regex.IsMatch(kv.Value));
-      return selected
-             .Map(kv => (kv.Key, kv.Value))
-             .ToList();
-    }
-
-    public static List<(string hex, string name)> Approximates(this RGB rgb, int top) {
-      List<(string hex, int len)> distances = HexToRgb.Map(kvp => (kvp.hex, kvp.rgb.Distance(rgb)))
-                                                      .ToList();
-      distances.Sort((a, b) => a.len - b.len);
-      return distances
-             .Take(top)
-             .Map(x => (x.hex, Pavtone.HexToName[x.hex]))
-             .ToList();
-    }
-    public static List<(string hex, string name)> Approximates(this HSL hsl, int top) {
-      List<(string hex, float len)> distances = HexToHsl.Map(kvp => (kvp.hex, (kvp.hsl.Distance(hsl))))
-                                                        .ToList();
-      distances.Sort((a, b) => a.len >= b.len ? 1 : -1);
-      return distances
-             .Take(top)
-             .Map(x => (x.hex, Pavtone.HexToName[x.hex]))
-             .ToList();
-    }
-
-    public static List<(string hex, string name)> Approximates(this RGB rgb, RGB epsilon) {
-      var distances = HexToRgb.FindAll(entry => entry.rgb.AlmostEqual(rgb, epsilon));
-      return distances
-             .Map(x => (x.hex, Pavtone.HexToName[x.hex]))
-             .ToList();
-    }
-    public static List<(string hex, string name)> Approximates(this HSL hsl, HSL epsilon) {
-      var distances = HexToHsl.FindAll(entry => entry.hsl.AlmostEqual(hsl, epsilon));
-      return distances
-             .Map(x => (x.hex, Pavtone.HexToName[x.hex]))
-             .ToList();
+  public static partial class Munsell {
+    private static Cuvette _fashion = null;
+    private static Cuvette _product = null;
+    private static Cuvette SelectCuvette(Domain domain) {
+      switch (domain) {
+        case Domain.Fashion: return _fashion ?? (_fashion = Cuvette.Build(Domain.Fashion.ToPalett()));
+        case Domain.Product: return _product ?? (_product = Cuvette.Build(Domain.Product.ToPalett()));
+        default: return new Cuvette();
+      }
     }
   }
 }
