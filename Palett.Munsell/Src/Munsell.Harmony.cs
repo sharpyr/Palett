d2ac4105a7760802947f3e5b619bd8ce8432df85
hoyeungw <hoyeungw@outlook.com>
2021-08-30 22:31:48 +0800

    rewrite RhodoneaFolios to improve accuracy

--- Palett.Munsell/Src/Munsell.Harmony.cs
+++ Palett.Munsell/Src/Munsell.Harmony.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using Aryth;
 using Aryth.Polar;
 using Veho.Sequence;
+using static System.Math;
 using RGB = System.ValueTuple<byte, byte, byte>;
 using HSL = System.ValueTuple<float, float, float>;
 using POLAR = System.ValueTuple<double, double>;
@@ -31,17 +34,40 @@ namespace Palett {
                       .DistinctBy(kv => kv.hex).ToList();
       return analogous;
     }
-    public static List<(string hex, string name)> RhodoneaFolios(this (float h, float s, float l) hsl, double saturationDeviation, int pedals, double density, Domain domain = Domain.Product) {
+
+    public static List<(string hex, string name)> RhodoneaFolios(this (float h, float s, float l) rimMark,
+                                                                 int petals,
+                                                                 double density = 0.01,
+                                                                 double lightMinimum = 0,
+                                                                 double saturTolerance = 18,
+                                                                 Domain domain = Domain.Product) {
       var cuvette = Munsell.SelectCuvette(domain);
-      var polar = hsl.HslToPolar();
-      var list = cuvette.HexToPolar.Map(x => x.polar); // create shallow copy
-      var rhodoneas = list.RhodoneaFolios(polar, pedals, density);
-      var saturationInterval = (hsl.s - saturationDeviation, hsl.s + saturationDeviation);
-      var polarEpsilon = (0.1, 0.1);
-      return cuvette
-             .HexToHsl
-             .FindAll(kv => rhodoneas.Exists(x => kv.hsl.AlmostEqual(x, polarEpsilon, saturationInterval)))
-             .Map(x => (x.hex, cuvette[x.hex]));
+      var polarMark = rimMark.HslToPolar();
+      var hexToHsl = cuvette.HexToHsl.Map(x => x); // create shallow copy
+
+      var saturInterval = (rimMark.s - saturTolerance, rimMark.s + saturTolerance);
+      var minL = (float)lightMinimum;
+
+      var area = PI * Pow(polarMark.r, 2) * (petals % 2 == 0 ? 0.5 : 0.25);
+      var maximum = (int)Round(density * area);
+      var thresholdPerPhase = maximum / petals;
+      // Console.WriteLine($">> [petals] {petals} [area] {area:N1} [maximum] {maximum} [threshold/phase] {thresholdPerPhase:00}");
+      var petalNote = PetalNote.Build(polarMark.θ, petals);
+      var target = new List<(string hex, float θ)>(maximum);
+      foreach (var (hex, (θ, s, r)) in hexToHsl.FiniteFlopper()) {
+        if (r < minL) continue;
+        if (!saturInterval.Has(s)) continue;
+        if (polarMark.FoliateRadius(θ, petals) < r) continue;
+        var phase = petalNote.Phase(θ);
+        if (thresholdPerPhase <= petalNote.Counter[phase]) continue;
+        petalNote.NotePhase(phase);
+        target.Add((hex, θ));
+        if (maximum <= petalNote.Sum) break;
+      }
+      return target
+             .OrderBy(x => x.θ)
+             .Select(x => (x.hex, cuvette[x.hex]))
+             .ToList();
     }
   }
 }
