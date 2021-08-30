317d8341ab38ac0fea9e0d4afd03b37668ee90cf
hoyeungw <hoyeungw@outlook.com>
2021-08-30 09:44:04 +0800

    add Domain

---
+++ Palett.Munsell/Src/Domain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Palett {
+  public enum Domain {
+    Fashion = 1,
+    Product = 2,
+  }
+
+  public static class DomainUtil {
+    public static Dictionary<string, string> ToPalett(this Domain domain) {
+      switch (domain) {
+        case Domain.Fashion: return Pavtone.Fashions;
+        case Domain.Product: return Pavtone.Products;
+        default: return new Dictionary<string, string>();
+      }
+    }
+  }
+}
