42164ed86d0b100c7b05ebaa894d09b0391287cc
hoyeungw <hoyeungw@outlook.com>
2021-08-31 11:40:15 +0800

    add Decos for palett related objects

---
+++ Palett.Deco/Src/Decos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Palett.Dye;
+using Veho.Sequence;
+
+// using Spare;
+// using Veho.Sequence;
+// using System.Collections.Generic;
+// using System.Linq;
+// using Palett.Fluos;
+// using Palett.Types;
+// using Spare.Padder;
+// using Texting.Enums;
+// using Texting.Joiner;
+// using Typen;
+// using Veho;
+// using Veho.Vector;
+
+namespace Palett {
+  public static class Decos {
+    public static readonly Dye<string> Dyer = DyeFactory.Hex();
+    public static string DecoPalett(this IReadOnlyList<(string hex, string name)> palett) {
+      return palett
+             .Map(x => {
+               return $"\n{x.hex}: {Dyer.Render(x.hex, x.name)} ({Conv.HexToHsl(x.hex)})";
+             })
+             .ToString();
+    }
+  }
+}
