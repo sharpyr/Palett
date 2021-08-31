4b00fe55f5c616f0d562e5cd30ef9a304038a98c
hoyeungw <hoyeungw@outlook.com>
2021-08-31 11:38:53 +0800

    renamed DyeFactory<T> to Dye<T>

---
+++ Palett.Dye/Src/Dye.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Palett.Types;
+using Palett.Utils.Ansi;
+using static Palett.Utils.Ansi.ControlCodes;
+using HSL = System.ValueTuple<float, float, float>;
+using RGB = System.ValueTuple<byte, byte, byte>;
+
+namespace Palett.Dye {
+
+
+  public class Dye<T> {
+    private string _head = "";
+    private string _tail = "";
+    private Func<T, string> _ansi;
+
+    public static Dye<T> Build(Func<T, string> ansi, params Effect[] effects) =>
+      new Dye<T> { _ansi = ansi }.AssignEffects(effects);
+
+    private Dye<T> AssignEffects(IEnumerable<Effect> effects) {
+      foreach (var effect in effects) {
+        var (h, t) = effect.EffectToAnsi();
+        _head += SC + h;
+        _tail += SC + t;
+      }
+      return this;
+    }
+
+    public (string, string) Toning(T color) {
+      var c = _ansi(color);
+      var h = L + _head + SC + c + R;
+      var t = L + _tail + R;
+      return (h, t);
+    }
+
+    public string Render(T color, string text) {
+      var (h, t) = Toning(color);
+      return h + text + t;
+    }
+
+    public Func<string, string> Make(T color) {
+      var (h, t) = Toning(color);
+      return text => h + text + t;
+    }
+  }
+}
--- Palett.Dye/Src/DyeFactory.cs
+++ Palett.Dye/Src/DyeFactory.cs
@@ -1,53 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using Palett.Types;
-using Palett.Utils.Ansi;
-using static Palett.Utils.Ansi.ControlCodes;
 using HSL = System.ValueTuple<float, float, float>;
 using RGB = System.ValueTuple<byte, byte, byte>;
 
 namespace Palett.Dye {
   public static class DyeFactory {
-    public static DyeFactory<RGB> Rgb(params Effect[] effects) => DyeFactory<RGB>.Build(AnsiProjector.RgbToAnsi, effects);
-    public static DyeFactory<HSL> Hsl(params Effect[] effects) => DyeFactory<HSL>.Build(AnsiProjector.HslToAnsi, effects);
-    public static DyeFactory<string> Hex(params Effect[] effects) => DyeFactory<string>.Build(AnsiProjector.HexToAnsi, effects);
+    public static Dye<RGB> Rgb(params Effect[] effects) => Dye<RGB>.Build(AnsiProjector.RgbToAnsi, effects);
+    public static Dye<HSL> Hsl(params Effect[] effects) => Dye<HSL>.Build(AnsiProjector.HslToAnsi, effects);
+    public static Dye<string> Hex(params Effect[] effects) => Dye<string>.Build(AnsiProjector.HexToAnsi, effects);
 
-    public static DyeFactory<Color> Color(params Effect[] effects) => DyeFactory<Color>.Build(AnsiProjector.ColorToAnsi, effects);
-  }
-
-  public class DyeFactory<T> {
-    private string _head = "";
-    private string _tail = "";
-    private Func<T, string> _ansi;
-
-    public static DyeFactory<T> Build(Func<T, string> ansi, params Effect[] effects) =>
-      new DyeFactory<T> {_ansi = ansi}.AssignEffects(effects);
-
-    private DyeFactory<T> AssignEffects(IEnumerable<Effect> effects) {
-      foreach (var effect in effects) {
-        var (h, t) = effect.EffectToAnsi();
-        _head += SC + h;
-        _tail += SC + t;
-      }
-      return this;
-    }
-
-    public (string, string) Toning(T color) {
-      var c = _ansi(color);
-      var h = L + _head + SC + c + R;
-      var t = L + _tail + R;
-      return (h, t);
-    }
-
-    public string Render(T color, string text) {
-      var (h, t) = Toning(color);
-      return h + text + t;
-    }
-
-    public Func<string, string> Make(T color) {
-      var (h, t) = Toning(color);
-      return text => h + text + t;
-    }
+    public static Dye<Color> Color(params Effect[] effects) => Dye<Color>.Build(AnsiProjector.ColorToAnsi, effects);
   }
 }
