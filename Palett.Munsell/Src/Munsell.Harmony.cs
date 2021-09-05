33eee91ede13a58ce6f9f57b136d144df1f94acb
hoyeungw <hoyeungw@outlook.com>
2021-09-05 20:40:58 +0800

    fix bugs for RhodoneaFolios

--- Palett.Munsell/Src/Munsell.Harmony.cs
+++ Palett.Munsell/Src/Munsell.Harmony.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Aryth;
 using Aryth.Polar;
+using Veho.Enumerable;
 using Veho.Sequence;
 using static System.Math;
 using RGB = System.ValueTuple<byte, byte, byte>;
@@ -40,32 +41,57 @@ namespace Palett {
                                                                  double lightMinimum = 0,
                                                                  double saturTolerance = 18,
                                                                  Domain domain = Domain.Product) {
-      var cuvette = Munsell.SelectCuvette(domain);
+     var cuvette = SelectCuvette(domain);
       var polarMark = rimMark.HslToPolar();
       var hexToHsl = cuvette.HexToHsl.Map(x => x); // create shallow copy
-
       var saturInterval = (rimMark.s - saturTolerance, rimMark.s + saturTolerance);
       var minL = (float)lightMinimum;
-
       var area = PI * Pow(polarMark.r, 2) * (petals % 2 == 0 ? 0.5 : 0.25);
       var maximum = (int)Round(density * area);
       var thresholdPerPhase = maximum / petals;
-      // Console.WriteLine($">> [petals] {petals} [area] {area:N1} [maximum] {maximum} [threshold/phase] {thresholdPerPhase:00}");
       var petalNote = PetalNote.Build(polarMark.θ, petals);
-      var target = new List<(string hex, float θ)>(maximum);
-      foreach (var (hex, (θ, s, r)) in hexToHsl.FiniteFlopper()) {
+      // Console.WriteLine($">> [petalNote marks] {petalNote.Marks.Deco()}");
+      var petalCache = Enumerable.Range(1, petals).ToDictionary(i => i, i => new SortedList<float, string>());
+      var sortedList = new SortedList<int, string>(maximum);
+      int HslIndicator((float h, float s, float l) hsl) => (int)hsl.h * 10000 + (int)hsl.s * 100 + (int)hsl.l;
+      foreach (var (hex, hsl) in hexToHsl.FiniteFlopper()) {
+        var (θ, s, r) = hsl;
         if (r < minL) continue;
-        if (!saturInterval.Has(s)) continue;
         if (polarMark.FoliateRadius(θ, petals) < r) continue;
         var phase = petalNote.Phase(θ);
         if (thresholdPerPhase <= petalNote.Counter[phase]) continue;
-        petalNote.NotePhase(phase);
-        target.Add((hex, θ));
+        if (saturInterval.Has(s)) {
+          petalNote.NotePhase(phase);
+          sortedList.Add(HslIndicator(hsl), hex);
+        } else {
+          var dS = Abs(rimMark.s - s);
+          var dR = Abs(rimMark.l - r);
+          var dθ = Distance(rimMark.h, θ);
+          petalCache[phase].Add(dS * 100 + dR + dθ / 360, hex);
+        }
         if (maximum <= petalNote.Sum) break;
       }
-      return target
-             .OrderBy(x => x.θ)
-             .Select(x => (x.hex, cuvette[x.hex]))
+      // Console.WriteLine($">> [sortedList.Count] {sortedList.Count}");
+      petalNote.Counter.Iterate(kv => {
+        var phase = kv.Key;
+        var count = kv.Value;
+        if (count < thresholdPerPhase) {
+          var cache = petalCache[phase];
+          if (cache?.Any() ?? false) {
+            foreach (var hex in cache.Values.Take(thresholdPerPhase - count)) {
+              sortedList.Add(HslIndicator(Conv.HexToHsl(hex)), hex);
+            }
+          }
+        }
+      });
+      // Console.WriteLine($">> [sortedList.Count] {sortedList.Count}");
+      // Console.WriteLine($">> [thresholdPerPhase] {thresholdPerPhase}");
+      // petalNote.Counter.Entries().DecoEntries().Says("petalNote");
+      // petalCache.Map((k, v) => (k, v.Count)).ToList().DecoEntries().Says("petalCacheNote");
+      sortedList.TrimExcess();
+      return sortedList
+             .Values
+             .Select(hex => (hex, cuvette[hex]))
              .ToList();
     }
   }
