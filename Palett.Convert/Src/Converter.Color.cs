af5f275f877ac645ee8194bfa77599ca215d8aee
hoyeungw <hoyeungw@outlook.com>
2021-09-27 10:42:39 +0800

    changed Color? to Color

--- Palett.Convert/Src/Converter.Color.cs
+++ Palett.Convert/Src/Converter.Color.cs
@@ -10,16 +10,16 @@ namespace Palett {
     public static string ColorToHex(this Color color) => "#" + ColorToInt(color).ToString("X6");
     public static (byte r, byte g, byte b) ColorToRgb(this Color color) => (color.R, color.G, color.B);
     public static (float h, float s, float l) ColorToHsl(this Color color) => (color.GetHue(), color.GetSaturation() * 100, color.GetBrightness() * 100);
-    
+
     public static Regex D3 => new Regex(
       @"\d{1,3}", RegexOptions.Compiled | RegexOptions.IgnoreCase
     );
 
-    public static Color? ParseColor(string hex, Space space = Space.Rgb) {
-      if (hex == null) return null;
+    public static Color ParseColor(string hex, Space space = Space.Rgb) {
+      if (hex == null) return Color.Empty;
       if (space == Space.Hex) return Conv.HexToColor(hex);
       var vec = D3.Matches(hex).Cast<Match>().Map(x => x.Value);
-      if (vec.Length == 0) return null;
+      if (vec.Length == 0) return Color.Empty;
       switch (space) {
         case Space.Rgb:
           return vec.Length < 3
@@ -27,9 +27,9 @@ namespace Palett {
             : (byte.Parse(vec[0]), byte.Parse(vec[1]), byte.Parse(vec[2])).RgbToColor();
         case Space.Hsl:
           return vec.Length < 3
-            ? (float.Parse(vec[0]), (float) 100, (float) 50).HslToColor()
+            ? (float.Parse(vec[0]), (float)100, (float)50).HslToColor()
             : (float.Parse(vec[0]), float.Parse(vec[1]), float.Parse(vec[2])).HslToColor();
-        default: return null;
+        default: return Color.Empty;
       }
     }
   }
