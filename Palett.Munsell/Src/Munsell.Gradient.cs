17e4fcc2f8f8fea293847769210b70e66ec244fb
hoyeungw <hoyeungw@outlook.com>
2021-10-29 10:47:19 +0800

    add class HSBGrad

--- Palett.Munsell/Src/Munsell.Gradient.cs
+++ Palett.Munsell/Src/Munsell.Gradient.cs
@@ -1,52 +1,89 @@
 ﻿using System;
 using Analys;
+using Aryth;
 using Palett.Types;
 using Veho;
 using static System.Math;
+using static Aryth.Math;
 using HSL = System.ValueTuple<float, float, float>;
 
 namespace Palett {
+  public class HSBGrad {
+    public HSB Basis;
+    public HSB Delta;
+    public HSBGrad(HSB basis, HSB delta) {
+      this.Basis = basis;
+      this.Delta = delta;
+    }
+
+    public float GradH(int i) => Restrict(this.Basis.H + i * this.Delta.H, 360);
+    public float GradS(int i) => Limit(this.Basis.S + i * this.Delta.S, 100);
+    public float GradB(int i) => Limit(this.Basis.B + i * this.Delta.B, 100);
+
+    public Func<int, float> MakeGrad(HSLAttr attr) {
+      switch (attr) {
+        case HSLAttr.H: return this.GradH;
+        case HSLAttr.S: return this.GradS;
+        case HSLAttr.L: return this.GradB;
+        default:        return null;
+      }
+    }
+  }
+
+  public static class HSBGrad2D {
+    public static Func<int, int, (float, float, float)> MakeGrad(this (HSBGrad gradX, HSBGrad gradY) gradPair, HSLAttr attrX, HSLAttr attrY) {
+      var (gradX, gradY) = gradPair;
+      Func<int, int, float> fnH = null, fnS = null, fnB = null;
+      switch (attrX) {
+        case HSLAttr.H:
+          fnH = (x, y) => gradX.GradH(x);
+          break;
+        case HSLAttr.S:
+          fnS = (x, y) => gradX.GradS(x);
+          break;
+        case HSLAttr.L:
+          fnB = (x, y) => gradX.GradB(x);
+          break;
+      }
+      switch (attrY) {
+        case HSLAttr.H:
+          fnH = (x, y) => gradY.GradH(y);
+          break;
+        case HSLAttr.S:
+          fnS = (x, y) => gradY.GradS(y);
+          break;
+        case HSLAttr.L:
+          fnB = (x, y) => gradY.GradB(y);
+          break;
+      }
+      // Console.WriteLine($">> (x, y) = ({x}, {y}) [grad] (x, y) = ({gradX.GradH(x)}, {gradY.GradH(y)})");
+      if (fnH == null) fnH = (x, y) => (float)Sqrt(gradX.GradH(x) * gradY.GradH(y));
+      if (fnS == null) fnS = (x, y) => (float)Sqrt(gradX.GradS(x) * gradY.GradS(y));
+      if (fnB == null) fnB = (x, y) => (float)Sqrt(gradX.GradB(x) * gradY.GradB(y));
+      return (x, y) => (fnH(x, y), fnS(x, y), fnB(x, y));
+    }
+  }
+
   public partial class Munsell {
     public static Crostab<HSL> GradientCrostab(
-      this ((float, float, float) aHSL, (float, float, float) bHSL) hslPair,
-      HSLDimension xDm, HSLDimension yDm,
-      int xCn, int yCn
+      this ((float, float, float) a, (float, float, float) b) hslPair,
+      HSLAttr attrX, HSLAttr attrY,
+      int lenX, int lenY
     ) {
-      var ((aH, aS, aL), (bH, bS, bL)) = hslPair;
-      var xdH = (bH - aH) / (xCn - 1);
-      var xdS = (bS - aS) / (xCn - 1);
-      var xdL = (bL - aL) / (xCn - 1);
-      var ydH = (bH - aH) / (yCn - 1);
-      var ydS = (bS - aS) / (yCn - 1);
-      var ydL = (bL - aL) / (yCn - 1);
+      var hsbA = HSB.FromTuple(hslPair.a);
+      var hsbB = HSB.FromTuple(hslPair.b);
+      var delta = hsbB - hsbA;
 
-      var fnX =
-        xDm == HSLDimension.H ? x => aH + x * xdH :
-        xDm == HSLDimension.S ? x => aS + x * xdS :
-        (Func<int, float>)(x => aL + x * xdL);
-      var fnY =
-        yDm == HSLDimension.H ? y => aH + y * ydH :
-        yDm == HSLDimension.S ? y => aS + y * ydS :
-        (Func<int, float>)(y => aL + y * ydL);
+      var gradX = new HSBGrad(hsbA, delta / (lenX - 1));
+      var gradY = new HSBGrad(hsbA, delta / (lenY - 1));
 
-      var fnH =
-        xDm == HSLDimension.H ? (x, y) => aH + x * xdH :
-        yDm == HSLDimension.H ? (x, y) => aH + y * ydH :
-        (Func<int, int, float>)((x, y) => (float)Sqrt((aH + x * xdH) * (aH + y * ydH)));
+      var fnSide = gradX.MakeGrad(attrX);
+      var fnHead = gradY.MakeGrad(attrY);
+      var fnRows = (gradX, gradY).MakeGrad(attrX, attrY);
 
-      var fnS =
-        xDm == HSLDimension.S ? (x, y) => aS + x * xdS :
-        yDm == HSLDimension.S ? (x, y) => aS + y * ydS :
-        (Func<int, int, float>)((x, y) => (float)Sqrt((aS + x * xdS) * (aS + y * ydS)));
-
-      var fnL =
-        xDm == HSLDimension.L ? (x, y) => aL + x * xdL :
-        yDm == HSLDimension.L ? (x, y) => aL + y * ydL :
-        (Func<int, int, float>)((x, y) => (float)Sqrt((aL + x * xdL) * (aL + y * ydL)));
-
-      var side = Vec.Init(xCn, x => fnX(x).ToString("F1"));
-      var head = Vec.Init(yCn, y => fnY(y).ToString("F1"));
-      var rows = (xCn, yCn).Init((x, y) => (fnH(x, y), fnS(x, y), fnL(x, y)));
+      var side = Vec.Init(lenX, x => fnSide(x).ToString("F1"));
+      var head = Vec.Init(lenY, y => fnHead(y).ToString("F1"));
+      var rows = Mat.Init((lenX, lenY), fnRows);
 
       return Crostab<HSL>.Build(side, head, rows);
     }
