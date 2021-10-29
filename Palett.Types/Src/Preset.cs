3cc56c563f1f7cd277cac4695a3134efcbfdf496
hoyeungw <hoyeungw@outlook.com>
2021-10-29 10:53:03 +0800

    minor fix

--- Palett.Types/Src/Preset.cs
+++ Palett.Types/Src/Preset.cs
@@ -2,14 +2,17 @@
 using HSL = System.ValueTuple<float, float, float>;
 
 namespace Palett.Types {
-  public class Preset {
+  public struct Preset {
     public string Min { get; set; }
     public string Max { get; set; }
     public string Na { get; set; }
 
-    // public static Preset Build(RGB min, RGB max, RGB na) { return new Preset {Min=min,Max=max,Na=na}; }
-    // public static Preset Build(HSL min, HSL max, HSL na) { return new Preset {Min=min,Max=max,Na=na}; }
-    public static Preset Build(string min, string max, string na = "#FFFFFF") => new Preset {Min = min, Max = max, Na = na};
-    // public static Preset Build(int min, int max, int na = 0) { return new Preset {Min=min,Max=max,Na=na}; }
+    public static Preset Build(string min, string max, string na = "#FFFFFF") {
+      return new Preset {
+                          Min = min,
+                          Max = max,
+                          Na = na
+                        };
+    }
   }
 }
